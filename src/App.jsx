c0fefb79c4504af2f95dcf28abcfea9ed01fa2d2
Nkugavarathan <kugavarathan28@gmail.com>
2025-03-31 08:29:19 +0530

    finished basic setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react"
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import Order from "./components/Order"
+import About from "./components/About"
+import Contact from "./components/Contact"
 export default function App() {
-  return <div></div>
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="order" element={<Order />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
